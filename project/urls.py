afa2306ea425659dd8aec5160e9dc24846e0fc5a
Vic-Nas <nasci.victorio@gmail.com>
2026-03-26 10:11:31 -0400

    feat: move login to root

--- project/urls.py
+++ project/urls.py
@@ -1,12 +1,13 @@
 # project/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 import project.views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dashboard.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('', include('accounts.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('billing/', include('billing.urls')),
     path('emails/', include('emails.urls')),
     path('legal/', include('project.legal_urls')),
