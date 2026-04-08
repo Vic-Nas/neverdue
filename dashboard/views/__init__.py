4cca3707180a7f21f42379ba811ed5caa527de28
Vic-Nas <nasci.victorio@gmail.com>
2026-04-07 20:21:33 -0400

    fix: missing import link

--- dashboard/views/__init__.py
+++ dashboard/views/__init__.py
@@ -1,5 +1,5 @@
 # dashboard/views/__init__.py
-from .events import index, event_detail, event_edit, event_delete
+from .events import index, event_detail, event_edit, event_delete, event_links
 from .actions import event_prompt_edit, events_bulk_action, export_events
 from .categories import categories, category_detail, category_edit, category_delete, categories_bulk_delete
 from .queue import queue, queue_status, queue_job_detail, queue_job_reprocess, queue_job_retry, queue_job_delete, queue_jobs_bulk_delete
